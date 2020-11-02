91ea1c42841dcde7637d9b15ad19e522c413079a
cc01748 <cc01748@surrey.ac.uk>
2020-11-02 18:47:30 +0000

    Fourth committ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#home'
+  get 'home/home'
+
+  get 'home/currentorders'
+  get 'home/oldorders'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get home_home_url
+  test "should get oldorders" do
+    get home_oldorders_url
     assert_response :success
   end
 
