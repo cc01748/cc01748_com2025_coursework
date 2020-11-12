b524692098f9e346df1c48c23e98a559c9a1c710
cc01748 <cc01748@surrey.ac.uk>
2020-11-12 18:49:10 +0000

    Added basic locales

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  
+#Tests for overview page
   test "should get overiew" do
     get home_overview_url
     assert_response :success
