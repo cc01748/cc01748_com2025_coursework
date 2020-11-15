1a3b320efd4f2c7f63801f651fec5a9e5122e208
cc01748 <cc01748@surrey.ac.uk>
2020-11-15 18:37:09 +0000

    Added JS file - added script to store and grab data from URL. Added some comments

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,4 +1,6 @@
+// Ready document
 $(document).ready(function() {
+    // When "go_to" button is clicked - redirect to vieworder with order number stored in the URL
     $("#go_to_button").click(function() {
         var url = $('#order_number').prop('innerHTML');
         
---
+++ app/assets/javascripts/vieworder.js
@@ -0,0 +1,9 @@
+// Ready document
+$(document).ready(function() {
+    // Grab current URL - split it on the order number stored in URL 
+    var currentUrl = window.location.href;
+    var urlLength = currentUrl.length;
+    var orderNumber = currentUrl.substring(38, urlLength);
+    
+    $('#order_number').text(orderNumber);
+});
