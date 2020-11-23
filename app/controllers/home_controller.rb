e876187eae6b4e4ad236b9dd788be7300c3d25ac
cc01748 <cc01748@surrey.ac.uk>
2020-11-23 18:39:45 +0000

    Tried nokogiri, decided against it. Finishing work for the day

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def vieworder
+    @orders = Order.all
+    
   end
 
   def contact
@@ -11,4 +13,6 @@ class HomeController < ApplicationController
   def orders 
     @orders = Order.all
   end
+
+
 end
