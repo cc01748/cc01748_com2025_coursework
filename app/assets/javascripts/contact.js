5a4d3e762a4a81c2776c287f5261ad407146aa40
cc01748 <cc01748@surrey.ac.uk>
2020-12-03 13:15:00 +0000

    Made jQuery empty field checker more accurate

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -1,9 +1,17 @@
 // Simple function to ensure no fields are left empty in form when submitted
-$('#submit_button').click(function(){
-    $('input').each(function() {
-        if(!$(this).val()){
-            alert('Empty fields detected');
-           return false;
+$(document).ready(function() {
+    $('#submit_button').click(function() {
+        var user = $('#formGroupUser').val();
+        var email = $('#formGroupEmail').val();
+        var message = $('#formGroupMessage').val();
+        if (user.length < 1) {
+            alert("Username field empty");
+        };
+        if (email.length < 1) {
+            alert("Email field empty");
+        };
+        if (message.length < 1) {
+            alert("Message field empty");
         }
     });
-});
+})
